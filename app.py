7d65e4519b92575e4c9d8738d845b0a9bcabf958
mwm-proyecto <mwm.proyecto@gmail.com>
2025-07-04 05:26:34 -0300

    cambios visuales_areas protegidas

--- app.py
+++ app.py
@@ -3,8 +3,13 @@ import streamlit as st
 import geemap.foliumap as geemap
 import ee
 
-st.set_page_config(layout="wide")
-st.title("MWM Mapping Wilderness Management")
+st.set_page_config(
+    page_title="MWM",
+    page_icon="🌲",
+    layout="wide"
+)
+st.markdown('<h1 style="color:green;">MWM</h1>', unsafe_allow_html=True)
+st.title("_Mapping Wilderness Management_")
 
 # Cargar directamente desde secrets
 service_account_info = st.secrets["GEE_JSON"]
@@ -15,6 +20,8 @@ credentials = ee.ServiceAccountCredentials(
 )
 ee.Initialize(credentials)
 
+st.header("Áreas Protegidas en Argentina")
+st.write("Argentina cuenta con un extenso Sistema Nacional de Áreas Protegidas (SNAP) que abarca diversas categorías como parques nacionales, reservas naturales y monumentos naturales, con el objetivo de proteger la biodiversidad y los ecosistemas del país. ")
 Map = geemap.Map(center=[-38.0, -64.0], zoom=4.5)
 
 paises = ee.FeatureCollection("USDOS/LSIB_SIMPLE/2017")
@@ -25,5 +32,8 @@ ap_argentina = areas_protegidas.filterBounds(argentina.geometry())
 
 Map.addLayer(argentina, {'color': 'blue'}, 'Límite Argentina')
 Map.addLayer(ap_argentina, {'color': 'green'}, 'Áreas protegidas (todo el país)')
+
+st.divider()
+st.header("Variación del NDVI en el Norte Argentino")
 
 Map.to_streamlit()
